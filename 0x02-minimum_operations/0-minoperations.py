8c5bc9501e2ac68400db5d78b57342302bac5119
YassineRKB <yassine@arcraven.com>
2024-01-16 01:01:48 +0100

    task 0: gonna wait for cheker to validate

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module for Minimum Operations"""
+
+
+def minOperations(n):
+    """calculates the fewest number of 
+    operations needed to result in exactly 
+    n H characters in the file."""
+    if n > 1:
+        i = 2
+        count = 0
+        while i <= n:
+            if n % i != 0:
+                i += 1
+            else:
+                count += i
+                n = n / i
+        return count
+    return 0
