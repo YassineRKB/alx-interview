688a192c380d01010d0503a8d9b5dac8d3ee4a38
YassineRKB <yassine@arcraven.com>
2024-02-01 03:30:24 +0100

    ensuring continuation of pattern

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,23 +3,43 @@
 
 
 def validUTF8(data):
-    """method for validating utf8 encoding"""
+    """Checks if a list of integers are valid UTF-8 codepoints."""
+    n = len(data)
     bCount = 0
-    for byte in data:
-        if bCount == 0:
-            if byte >> 7 == 0:
-                bCount = 0
-            elif byte >> 5 == 0b110:
-                bCount = 1
-            elif byte >> 4 == 0b1110:
-                bCount = 2
-            elif byte >> 3 == 0b11110:
-                bCount = 3
+    for i in range(n):
+        if bCount > 0:
+            bCount -= 1
+            continue
+        if data[i] <= 0x7F:
+            bCount = 0
+        elif data[i] & 0b11111000 == 0b11110000:
+            step = 4
+            if n - i >= step:
+                next_body = all((data[j] & 0b11000000 == 0b10000000) for j in range(i + 1, i + step))
+                if not next_body:
+                    return False
+                bCount = step - 1
+            else:
+                return False
+        elif data[i] & 0b11110000 == 0b11100000:
+            step = 3
+            if n - i >= step:
+                next_body = all((data[j] & 0b11000000 == 0b10000000) for j in range(i + 1, i + step))
+                if not next_body:
+                    return False
+                bCount = step - 1
+            else:
+                return False
+        elif data[i] & 0b11100000 == 0b11000000:
+            step = 2
+            if n - i >= step:
+                next_body = all((data[j] & 0b11000000 == 0b10000000) for j in range(i + 1, i + step))
+                if not next_body:
+                    return False
+                bCount = step - 1
             else:
                 return False
         else:
-            if byte >> 6 != 0b10:
-                return False
-            bCount -= 1
-    return bCount == 0
+            return False
+    return True
 
